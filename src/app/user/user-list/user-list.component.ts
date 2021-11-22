8d23637a2415cfb3fdd938b609329043cedd4a5b
Michael Walker <walkerrepair255@yahoo.com>
2021-11-22 09:51:33 -0500

    fix/added update to subscribe method

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -5,30 +5,26 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-
   users: User[] = [
-    new User(1, "ToddTodders", "Todd", "Admin"),
-    new User(2, "ScottScotters", "Scott", "Reviewer"),
-    new User(3, "BobBobbers", "Bob", "User")
+    new User(1, 'ToddTodders', 'Todd', 'Admin'),
+    new User(2, 'ScottScotters', 'Scott', 'Reviewer'),
+    new User(3, 'BobBobbers', 'Bob', 'User'),
   ];
 
-  constructor(
-    private usersvc: UserService
-  ) { }
+  constructor(private usersvc: UserService) {}
 
   ngOnInit(): void {
-    this.usersvc.list().subscribe(
-      res => {
-        console.debug("Users:", res)
+    this.usersvc.list().subscribe({
+      next: (res) => {
+        console.debug('Users:', res);
         this.users = res;
       },
-      err => {
+      error: (err) => {
         console.error(err);
-      }
-    );
+      },
+    });
   }
-
 }
